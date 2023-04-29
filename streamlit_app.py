12e5c7139607ff5e1f87316ab962d4859d0859aa
jmonnette <jmonnette@gmail.com>
2023-04-29 14:19:39 -0400

    Update streamlit_app.py
    Updated menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
 streamlit.title("This is a test")
-streamlit.header("Menu Items")
-streamlit.text("Burger")
-streamlit.text("Fries")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
